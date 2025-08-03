16352b35f56d18add1d7303ace3bcb844b34d2be
Pascal Brand <pascal.brand38@gmail.com>
2025-08-03 08:29:47 +0200

    layerFromName: strong types, and michelin
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- src/layerFromName.ts
+++ src/layerFromName.ts
@@ -3,17 +3,35 @@
 
 import type { TileLayerOptions } from "leaflet"
 
+/** type guessed by a layer name of type ```LayerNamesType```,
+ * such as ```Google&type=street&lang=en```
+ */
 export interface LayerFromNameType {
+  /** guessed tileayer url */
   tileLayer: string,
+  /** guessed tilelayer options (subdomains,...) */
   options: TileLayerOptions,
 }
 
-// TODO: types should include lang,... and be as restrictive as possible
-export type LayerNamesType =
-  'OSM' |
-  'Google' |
-  `Google&type=${'satellite' | 'street' | 'hybrid' | 'terrain' }`
+type _char = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' |
+             'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z'
 
+type _googleTypeType = '' | `&type=${'satellite' | 'street' | 'hybrid' | 'terrain '}`
+type _googleLangType = '' | `&lang=${_char}${_char}`
+type _michelinTypeType = '' | `&type=${'map' | 'label'}`
+
+/** tilelayer name type */
+export type LayerNamesType =
+  /** openstreetmap */
+  'OSM' |
+  /** google map */
+  `Google${_googleTypeType}${_googleLangType}` |
+  /** michelin */
+  `Michelin${_michelinTypeType}`
+
+/** return url tile layer and tile layer options related to a layer name,
+ * such as ```Google&type=street&lang=en```
+ */
 export function getLayerOptionsFromName(name: LayerNamesType): LayerFromNameType {
   // Iterating the search parameters
   // cf. https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams
@@ -35,7 +53,7 @@ export function getLayerOptionsFromName(name: LayerNamesType): LayerFromNameType
   }
 
   if (name.startsWith('Google')) {
-    const corresp = { 'satellite': 's', 'street': 'm', 'terrain': 'p', 'hybrid': 's,h', }
+    const corresp = { 'satellite': 's', 'street': 'm', 'terrain': 'p', 'hybrid': 's,h' }
     let lyrs = '&lyrs=s'    // default is satellite view
     let lang = ''           // default does not define any language
 
@@ -58,16 +76,39 @@ export function getLayerOptionsFromName(name: LayerNamesType): LayerFromNameType
     }
   }
 
-  return getLayerOptionsFromName('Google&type=street')   // default return
+
+  if (name.startsWith('Michelin')) {
+    // from https://xyz.michelstuyts.be/service.php?id=620&lang=fr
+    let tileLayer = 'https://map1.viamichelin.com/map/mapdirect?map=viamichelin&z={z}&x={x}&y={y}&format=png&version=202301111200&layer=background&protocol=https'
+    if (searchParams.has('type') && (searchParams.get('type')==='label')) {
+			tileLayer = 'https://{s}.viamichelin.com/map/mapdirect?map=hybrid&z={z}&x={x}&y={y}&format=png&version=201503191157&layer=network'
+    }
+    return {
+      tileLayer,
+      options: {
+        opacity: 1,
+        subdomains:[ 'map1', 'map2', 'map3', 'map4', 'map5', ],
+        attribution: '&copy; Michelin',
+        minZoom: 5,
+        maxZoom: 19,
+      },
+    }
+  }
+
+  return getLayerOptionsFromName('OSM')   // default return
 }
 
-
-export function mergeTileLayerOptions(providedOptions: TileLayerOptions | undefined, guessedOptions: TileLayerOptions): TileLayerOptions {
-  if (!providedOptions) {
-    providedOptions = {}
-  }
-  Object.keys(guessedOptions).forEach(key =>
-    providedOptions[key] = providedOptions[key] || guessedOptions[key]
-  )
-  return providedOptions
+/** Merge TileLayer options that are given by the users, with the one guessed by tileByName */
+export function mergeTileLayerOptions(
+  /** options given by the users */
+  providedOptions: TileLayerOptions | undefined,
+  /** options guessed by tileByName */
+  guessedOptions: TileLayerOptions): TileLayerOptions {
+    if (!providedOptions) {
+      providedOptions = {}
+    }
+    Object.keys(guessedOptions).forEach(key =>
+      providedOptions[key] = providedOptions[key] || guessedOptions[key]
+    )
+    return providedOptions
 }
