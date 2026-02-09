bbf2c0bdad849636bb293ce047304f66e3ebcd6e
Pascal Brand <pascal.brand38@gmail.com>
2026-02-09 10:47:43 +0100

    biome and prettier
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

---
+++ prettier.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import("prettier").Config} */
+export default {
+  endOfLine: 'lf',
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: ['**/*.astro'],
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};
